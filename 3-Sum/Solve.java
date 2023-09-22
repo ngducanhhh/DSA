6a0968472ddaadd7184f3ff2c538b569bfd1dea8
ngducanhhh <nguyenducanh2925@gmail.com>
2023-09-22 11:40:36 +0700

    Resolved 3-Sum

--- 3-Sum/Solve.java
+++ 3-Sum/Solve.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 public class Solve {
+    public static int bn(int[] a, int l, int r, int u) {
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(a[mid]==u) return mid;
+            else if(a[mid]<u) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
@@ -8,12 +20,10 @@ public class Solve {
             a[i] = input.nextInt();
         }
         int cnt = 0;
-        // Do phuc tap cua thuat toan la O(n^3)
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                for(int k=0; k<n; k++) {
-                    if(a[i] + a[j] + a[k]==0) cnt++;
-                }
+        for(int i=0; i<=n-3; ++i) {
+            for(int j=i+1; j<=n-2; ++j) {
+                int k = bn(a, j+1, n-1, -a[i]-a[j]);
+                if(k!=-1) cnt++;
             }
         }
         System.out.println(cnt);
