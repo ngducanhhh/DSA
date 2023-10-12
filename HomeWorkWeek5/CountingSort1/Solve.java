813824adb222ef1782dc47e84326c6d2d09d9346
ngducanhhh <nguyenducanh2925@gmail.com>
2023-10-12 20:20:17 +0700

    Solved Counting Sort 1

---
+++ HomeWorkWeek5/CountingSort1/Solve.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class Solve {
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        int n = 100;
+        List<Integer> cnt = new ArrayList<>();
+        cnt.addAll(Collections.nCopies(n, 0));
+        for(int i : arr) {
+            int res = cnt.get(i);
+            cnt.set(i, res+1);
+        }
+        return cnt;
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Solve.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
